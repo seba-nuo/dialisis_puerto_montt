f5c148e9eea51d24fa29f0100252c0c9ee11213e
figo1994 <felipe.igor.y@gmail.com>
2021-04-10 23:59:08 -0400

    insert Img

--- frontend-dialisis/src/pages/Inicio/Inicio.js
+++ frontend-dialisis/src/pages/Inicio/Inicio.js
@@ -1,9 +1,19 @@
 
+import images from "../../Images";
 
 const Inicio = () => {
     return(
+
+        <> 
+        <img scr={images} alt="img"/> 
+        
         <p>Quienes Somos..</p>
+        </>
+
+
     )
+
+    
 }
 
 export default Inicio
