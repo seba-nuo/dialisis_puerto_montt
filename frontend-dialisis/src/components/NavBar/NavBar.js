9bf012cc6fa9561da46e30614e5278d90c3d5f01
figo1994 <felipe.igor.y@gmail.com>
2021-04-25 21:15:25 -0400

    styles, Add Page & info

--- frontend-dialisis/src/components/NavBar/NavBar.js
+++ frontend-dialisis/src/components/NavBar/NavBar.js
@@ -5,6 +5,9 @@ const NavBar = () => {
         <Nav>
             <NavLink to="/">Inicio</NavLink>
             <NavLink to="/acerca">Acerca</NavLink>
+            <NavLink to="/profesionañes">Profesionales</NavLink>
+            <NavLink to="/galeria">Galería</NavLink>
+            <NavLink to="/pacientes">Pacientes</NavLink>
             <NavLink to="/contacto">Contacto</NavLink>
         </Nav>
     )
