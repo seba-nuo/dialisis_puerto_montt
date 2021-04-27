d147a79a6c76de2288e4a439425c431ef4ad24b3
ghostGL <gustavo.barria04@inacapmail.cl>
2021-04-26 22:59:05 -0400

    contatto

--- frontend-dialisis/src/pages/Contacto/Contacto.js
+++ frontend-dialisis/src/pages/Contacto/Contacto.js
@@ -1,17 +1,39 @@
-import React from 'react'
-
+import React,{ components } from 'react'
 
 const Contacto = () => {
-    return(
-        <body>
-            <center><h1>Contacto</h1></center> 
-             <form>
-                    <label>nombre</label>
-
-             </form>             
-        </body>
-
-
+    return(      
+        <>
+            <div>
+                <row>
+                    <h1>Contacto</h1>   
+                     <form >
+                        <div >
+                             <label className="nombre" >Nombre
+                                <input type="text" name="nombre" id="nombre" className="form-control"/>
+                                </label>
+                           </div><div>
+                                <label>Apellido
+                                <input type="text" name="apellido" id="apellido" className="form-control"/>
+                                </label>
+                            </div><div>
+                             <label className="correo">Correo
+                             <input type="text" name="correo" id="correo" className="form-control"/>
+                             </label>
+                            </div><div>
+                             <label>Telefono
+                             <input type="text" name="telefono" id="telefono" className="form-control"/>
+                             </label>
+                            </div><div>            
+                                <label text="comentario">Comentario </label><br/>
+                                <textarea></textarea>
+                               
+                            </div><div>
+                             <button>Enviar</button>
+                            </div>
+                    </form>                                                 
+                 </row>
+            </div>
+        </>
     )
 }
 
