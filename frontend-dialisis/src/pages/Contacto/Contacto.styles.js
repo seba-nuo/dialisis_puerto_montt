8e31420eec5be75e069192911ca0b7f5b7116e9d
ghostGL <gustavo.barria04@inacapmail.cl>
2021-05-10 17:16:46 -0400

    contacto todavia falta

--- frontend-dialisis/src/pages/Contacto/Contacto.styles.js
+++ frontend-dialisis/src/pages/Contacto/Contacto.styles.js
@@ -8,13 +8,13 @@ export const sharedStyles= css`
     padding: 20px;
     box-sizing: border-box;
     
-`
+`;
 export const SDiv = styled.div`
     display:flex;
-    justify-content: center;
+    justify-content: left;
     align-items:center;
-    padding:30 20px;
-`
+    padding: 5px;
+`;
 export const SForm = styled.form`
     width: 100%;
     max-width: 700px;
@@ -23,21 +23,21 @@ export const SForm = styled.form`
     border-radius: 10px;
     box-sizing:border-box;
     box-shadow:3 3px 20px 3px rgba(0,0,0,0.2);
-`
+`;
 export const SInput = styled.input`
     display:block;
     width:380px;
     border-color:black;
     
     ${sharedStyles}
-`
+`;
 export const STextArea = styled.textarea`
     background-color: white;
     width: 380px;
     min-height:150px;
     resize:none;
     ${sharedStyles}
-`
+`;
 export const SButton = styled.button`
     display: block;    
     font-weight: bold;
@@ -45,11 +45,11 @@ export const SButton = styled.button`
     border:3px solid black;
     border-radius:10px;
     cursor: pointer;
-`
+`;
 export const SLabel = styled.label`
 color: black;
 padding:20px; 
 font-weight: bold;
 font-family:Arial;
 
-`
+`;
