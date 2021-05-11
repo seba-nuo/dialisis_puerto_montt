6af36d6fbf9f9cf9f7df3b88321b51a985e5ca22
figo1994 <felipe.igor.y@gmail.com>
2021-05-11 17:38:09 -0400

    edit page personal

--- frontend-dialisis/src/pages/Personal/Personal.js
+++ frontend-dialisis/src/pages/Personal/Personal.js
@@ -1,6 +1,28 @@
+import React from 'react';
+import icono from '../../Images/Icono.png';
+
 const Personal = () => {
-    return(
-        <p>Personal</p>
+    return (
+
+
+
+        <div> <center>
+
+         <div class="grid-container">
+                <div class="icono1" >   <img width= '100px´' height= '80px' src={icono}  alt='icono 1'text= "Personal1" />  </div>
+                <div class="icono2">Personal 2</div>
+                <div class="icono3">Personal 3</div>
+                <div class="icono4">Personal 4</div>
+                <div class="icono5">Personal 5</div>
+                <div class="icono6">Personal 6</div>
+                <div class="icono7">Personal 7</div>
+            </div>
+
+
+        </center></div>
+
+
+
     )
 }
 
