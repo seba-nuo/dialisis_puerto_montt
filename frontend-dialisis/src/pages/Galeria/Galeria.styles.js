032e70bcb76931d7209a515d50eb7ec5f6cfb3fe
ghostGL <gustavo.barria04@inacapmail.cl>
2021-05-17 15:27:44 -0400

    contacto y galeria, faltan detalles

---
+++ frontend-dialisis/src/pages/Galeria/Galeria.styles.js
@@ -0,0 +1,14 @@
+import styled, { css } from "styled-components";
+
+export const SDiv = styled.div`
+    display:flex;
+    justify-content: left;
+    align-items:center;
+    padding: 5px;
+`;
+export const Simg = styled.img`
+    width:600px;
+    height:300px;
+    margin:auto;
+    padding:50px;
+`;
