2c4e52bfcd77bfd48d093530eb9cd19cf91e940e
seba-nuo <sebas.a.nunez@gmail.com>
2021-05-21 23:56:38 -0400

    fix test not working in windows

--- backend-dialisis/test/app.test.js
+++ backend-dialisis/test/app.test.js
@@ -8,21 +8,6 @@ beforeAll(async (done) => {
   done();
 });
 
-/** this code is called once before all the tested are finished */
-afterAll(async (done) => {
-
-  const dbSettings = strapi.config.get("database.connections.default.settings");
-
-   //delete test database after all tests
-  // if (dbSettings && dbSettings.filename) {
-  //   const tmpDbFile = `${__dirname}/../${dbSettings.filename}`;
-  //   if (fs.existsSync(tmpDbFile)) {
-  //     fs.unlinkSync(tmpDbFile);
-  //   }
-  // }
-  done();
-});
-
 describe("Strapi in general", () => {
   it("strapi is defined", async (done) => {
     expect(strapi).toBeDefined();
--- backend-dialisis/test/noticias/index.js
+++ backend-dialisis/test/noticias/index.js
@@ -2,16 +2,17 @@ const request = require("supertest")
 
 
 describe('POST /auth/local/register', () => {
-    it('deberia registrar un usuario nuevo', async done => {
-        await request(strapi.server)
+    it('deberia registrar un usuario nuevo', done => {
+        request(strapi.server)
             .post('/auth/local/register')
             .send({
                 username: 'test',
                 email: 'test@tst.cl',
                 password: 'password'
             })
+            .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            // .expect(200)
+            .expect(200, done)
         
         //     console.log('User profile', response.data.user);
         //     console.log('User token', response.data.jwt);
