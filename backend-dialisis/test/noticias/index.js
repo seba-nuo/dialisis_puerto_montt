d26f8c818600a5f0e13b307807f528d6f26db947
seba-nuo <sebas.a.nunez@gmail.com>
2021-05-24 23:23:16 -0400

    testing noticias endpoint

--- backend-dialisis/test/noticias/index.js
+++ backend-dialisis/test/noticias/index.js
@@ -1,20 +1,37 @@
 const request = require("supertest")
 
+// user mock data
+const mockUserData = {
+    username: 'tester',
+    email: 'tester@strapi.com',
+    provider: 'local',
+    password: '1234abc',
+    confirmed: true,
+    blocked: null,
+};
 
-describe('POST /auth/local/register', () => {
-    it('deberia registrar un usuario nuevo', done => {
-        request(strapi.server)
-            .post('/auth/local/register')
-            .send({
-                username: 'test',
-                email: 'test@tst.cl',
-                password: 'password'
-            })
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200, done)
-        
-        //     console.log('User profile', response.data.user);
-        //     console.log('User token', response.data.jwt);
+describe('GET /noticias', () => {
+    it('deberia retornar una lista de noticias', async done => {
+
+        const validData = await strapi.entityValidator.validateEntityCreation(
+            strapi.models.noticia,
+            {
+                "titulo": "string",
+                "cuerpo": "string",
+                "fecha": "2021-05-24",
+                "url": "uid"
+            }
+          );
+
+        const noticia = await strapi.query('noticia').create(validData);
+
+        console.log(noticia);
+
+        // TODO: revisar como actualizar los permisos del usuario publico
+
+        // await request(strapi.server)
+        //         .get('/noticias')
+        //         .expect(200)
+        done()
     })
 })
