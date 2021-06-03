ccfa7880d70cc30c007638c2a5c593db8d7db568
figo1994 <felipe.igor.y@gmail.com>
2021-06-03 12:06:34 -0400

    backgraground Page Inicio.js

--- frontend-dialisis/src/pages/Inicio/Inicio.styles.js
+++ frontend-dialisis/src/pages/Inicio/Inicio.styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+
 export const Titulo = styled.h1`
 position: Center;
 color: blue;
@@ -14,3 +15,20 @@ text-align: center;
 max-width: 700px;
 
 `
+
+export const Columna = styled.p`
+columns: 2;
+
+`
+export const Fondo = styled.div`
+background-image: url("https://images.pexels.com/photos/1103970/pexels-photo-1103970.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+width:100%;
+height: 100vh;
+object-fit: contain;
+/* box-shadow:inset 0 0 0 1000px rgba(0, 0, 0, 0.6) */
+`
+
+
+
+
+
