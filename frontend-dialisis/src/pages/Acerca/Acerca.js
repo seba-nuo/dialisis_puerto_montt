00da1072130057feaa86aac886fd39c1871e907d
Sebastian N <sebas.a.nunez@gmail.com>
2021-07-06 20:50:16 -0400

    acerca responsivo

--- frontend-dialisis/src/pages/Acerca/Acerca.js
+++ frontend-dialisis/src/pages/Acerca/Acerca.js
@@ -1,24 +1,16 @@
-import React from 'react'; 
-import {Titulo2} from '../../pages/Acerca/Acerca.styles'
-import Logo1 from '../../Images/logo1.jpeg'
-import{Fondo1,Fondo2,Cuerpo,Columna} from '../Acerca/Acerca.styles'
+import React from 'react';
+// import Logo1 from '../../Images/logo1.jpeg'
+import { FondoMision, FondoVision, Cuerpo, Titulo, Container } from '../Acerca/Acerca.styles'
 // import {FondoAcerca} from '../../pages/Acerca/Acerca.styles'
 
 
 const Acerca = () => {
 
-
-    return(
-        <>
-    
-        
-
-       <center>
-           <img src={Logo1}  alt='logo 1'/>
-       </center>
-       <Columna>
-       <Fondo1>
-       <center> <Titulo2>Misión</Titulo2></center>
+    return (
+        <Container>
+            {/* <img src={Logo1} alt='logo 1' /> */}
+                <FondoMision>
+                    <Titulo>Misión</Titulo>
                     <Cuerpo>
                         Centro de Diálisis, con largos años de experiencia que proporciona una terapia de sustitución renal
                         a pacientes con enfermedad renal crónica, garantizando una atención segura a través de la conformación de nuestro
@@ -27,11 +19,10 @@ const Acerca = () => {
                         Nuestros procesos asistenciales se rigen según las normas del Manual Estándar General de Acreditación para los Centros
                         de Diálisis.
                     </Cuerpo>
-       </Fondo1>
-      
-
-        <Fondo2>
-        <center> <Titulo2>Visión</Titulo2></center>
+                </FondoMision>
+                
+                <FondoVision>
+                    <Titulo>Visión</Titulo>
                     <Cuerpo>
                         Ser un Centro de Diálisis acreditado como entidad prestadora, reconocido por otorgar una prestación de hemodiálisis
                         con altos estándares de calidad, eficiencia y seguridad, centrados en el paciente, en un ambiente de trabajo en equipo,
@@ -39,13 +30,8 @@ const Acerca = () => {
 
                     </Cuerpo>
 
-        </Fondo2>
-        </Columna>
-      
-   
-        
-        </>
-        
+                </FondoVision>
+        </Container>
     )
 }
 
